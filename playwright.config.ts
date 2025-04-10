1d590b567102895315224e68e0e3c3ec8990c69a
ftoire <ftoire.pro@gmail.com>
2025-04-10 10:13:53 +0200

    Add playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './TEST',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'junit',
+  use: {
+    baseURL: 'https://to-scooby-do-b1b9c.web.app',
+    trace: 'on-first-retry',
+    headless: false,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+}); 
